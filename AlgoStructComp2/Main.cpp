9d10788c36289b32dbe40e8f5145320125ccfc55
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-09-05 13:40:10 +0200

    Update Main.cpp

--- AlgoStructComp2/Main.cpp
+++ AlgoStructComp2/Main.cpp
@@ -10,12 +10,13 @@ int main(int argc, char* argv[])
 {
     //Longer ut more clear method
     MultiLinkedList<int> IntList{2,1};
-    for (int I{}; I < IntList.GetSize(); ++I)
-        std::cout << IntList[I] << " ";
+    // for (int I{}; I < IntList.GetSize(); ++I)
+    //     std::cout << IntList[I] << " ";
+    std::cout << IntList << std::endl;
     std::cout << std::endl << "Size: " << IntList.GetSize() << std::endl;
   
 
-    IntList.InsertEnd(1);
+    // IntList.InsertEnd(1);
 
 
     for (int I{}; I < IntList.GetSize(); ++I)
