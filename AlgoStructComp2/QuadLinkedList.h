53d879930239fe3f53629b2f7d644ff97f025228
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-09-07 14:15:00 +0200

    Update QuadLinkedList.h

--- AlgoStructComp2/QuadLinkedList.h
+++ AlgoStructComp2/QuadLinkedList.h
@@ -9,17 +9,26 @@ template <typename T>
 class QuadLinkedList
 {
 private:
-    Node<T> Head{nullptr}, Tail{nullptr}, CurrentNode{nullptr};
+    Node<T>* Head{nullptr}, Tail{nullptr}, CurrentNode{nullptr};
     size_t Size_{};
     
 public:
     QuadLinkedList();
+    explicit QuadLinkedList(T NewValue);
     ~QuadLinkedList();
 };
 
 template <typename T>
 QuadLinkedList<T>::QuadLinkedList()
 {
+}
+
+template <typename T>
+QuadLinkedList<T>::QuadLinkedList(T NewValue)
+{
+    Head = Tail = CurrentNode = new Node<T>(NewValue);
+    
+    Size_++;
 }
 
 template <typename T>
