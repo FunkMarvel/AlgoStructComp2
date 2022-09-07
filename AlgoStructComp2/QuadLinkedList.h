08f0d5c14e02687cdea415294bd3281d7d6ff7c8
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-09-07 14:09:25 +0200

    Added quadlinkedlist file

---
+++ AlgoStructComp2/QuadLinkedList.h
@@ -0,0 +1,24 @@
+﻿
+#include "Node.h"
+
+template <typename T>
+class QuadLinkedList
+{
+private:
+    Node<T> Head{nullptr}, Tail{nullptr}, CurrentNode{nullptr};
+    size_t Size_{};
+    
+public:
+    QuadLinkedList();
+    ~QuadLinkedList();
+};
+
+template <typename T>
+QuadLinkedList<T>::QuadLinkedList()
+{
+}
+
+template <typename T>
+QuadLinkedList<T>::~QuadLinkedList()
+{
+}
