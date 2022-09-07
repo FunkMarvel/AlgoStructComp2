0609a8aae8a73b869e92166030a06943b1a1f126
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-09-07 14:19:06 +0200

    Update QuadLinkedList.h

--- AlgoStructComp2/QuadLinkedList.h
+++ AlgoStructComp2/QuadLinkedList.h
@@ -18,11 +18,18 @@ public:
     ~QuadLinkedList();
 };
 
+/**
+ * \brief Construct empty list.
+ */
 template <typename T>
 QuadLinkedList<T>::QuadLinkedList()
 {
 }
 
+/**
+ * \brief Construct list with single node.
+ * \param NewValue Data to store in initial node.
+ */
 template <typename T>
 QuadLinkedList<T>::QuadLinkedList(T NewValue)
 {
@@ -31,6 +38,9 @@ QuadLinkedList<T>::QuadLinkedList(T NewValue)
     Size_++;
 }
 
+/**
+ * \brief Delete all nodes and free data.
+ */
 template <typename T>
 QuadLinkedList<T>::~QuadLinkedList()
 {
