95bcea1059d88c6e718a104976156188e37be302
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-09-07 16:25:59 +0200

    Update QuadLinkedList.h

--- AlgoStructComp2/Node.h
+++ AlgoStructComp2/Node.h
@@ -2,12 +2,21 @@
 
 template <typename T>
 struct Node {
+private:
     Node* RightNode{nullptr};
     Node* LeftNode{nullptr};
     Node* UpNode{nullptr};
     Node* DownNode{nullptr};
     T Value{};
 
+public:
+    Node* GetRight() {return RightNode;}
+    Node* GetLeft() {return LeftNode;}
+    Node* GetUp() {return UpNode;}
+    Node* GetDown() {return DownNode;}
+
+    T& operator*(Node<T>* SomeNode) {return SomeNode.Value;}
+    
     Node();
     explicit Node(T NewValue);
 };
