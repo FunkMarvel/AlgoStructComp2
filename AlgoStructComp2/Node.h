1370da4a346074ee5b0d59c7da95b33deb5c39af
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-09-15 15:41:15 +0200

    Update Node.h

--- AlgoStructComp2/Node.h
+++ AlgoStructComp2/Node.h
@@ -1,35 +1,58 @@
 ﻿#pragma once
 
-template <typename T>
-struct Node {
-
-    Node* Link1{nullptr};
-    Node* Link2{nullptr};
-    Node* Link3{nullptr};
-    Node* Link4{nullptr};
-    T Value{};
-    
-    Node();
-    explicit Node(T NewValue);
-};
-
-template <typename T>
-Node<T>::Node() {
-    Value = T();
-}
-
-template <typename T>
-Node<T>::Node(T NewValue) {
-    Value = NewValue;
-}
-
-/**
+namespace Node
+{
+    /**
  * \brief Enum representing possible sides of node.
  */
-enum Direction
-{
-    Link1,
-    Link2,
-    Link3,
-    Link4
-};
+    enum Direction
+    {
+        Link1,
+        Link2,
+        Link3,
+        Link4
+    };
+
+
+    template <typename T>
+    class Node {
+
+        Node* Pointer1{nullptr};
+        Node* Pointer2{nullptr};
+        Node* Pointer3{nullptr};
+        Node* Pointer4{nullptr};
+
+    public:
+        unsigned short NumberOfLinks{};
+        T Value{};
+
+        Node*& GetLink(Direction dir)
+        {
+            switch (dir)
+            {
+            case Link1:
+                return Pointer1;
+            case Link2:
+                return Pointer2;
+            case Link3:
+                return Pointer3;
+            case Link4:
+                return Pointer4;
+            }
+            return nullptr;
+        }
+    
+        Node();
+        explicit Node(T NewValue);
+    };
+
+    template <typename T>
+    Node<T>::Node() {
+        Value = T();
+    }
+
+    template <typename T>
+    Node<T>::Node(T NewValue) {
+        Value = NewValue;
+    }
+}
